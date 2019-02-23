e349ccd53af1160416277d3c0682b15607280c44
Adolfho Athyla <adolfo.athyla@gmail.com>
2019-02-23 15:34:00 -0300

    Home

---
+++ PageControlDemo/Modules/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  PageControlDemo
+//
+//  Created by Adolfho Athyla on 23/02/19.
+//  Copyright © 2019 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
